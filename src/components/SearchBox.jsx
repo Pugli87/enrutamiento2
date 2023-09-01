e2cdbcf0c310296652ec9da53b598ad2ed2bafc8
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-08-31 23:39:57 -0500

    json y carpeta styled component

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,14 +1,14 @@
-import { Wrapper, Input, Icon } from "./SearchBox.styled";
+import { Wrapper, Input, Icon } from "../styledComponent/SearchBox.styled";
 
 export const SearchBox = ({ value, onChange }) => {
-  return (
-    <Wrapper>
-      <Icon />
-      <Input
-        type="text"
-        value={value}
-        onChange={(e) => onChange(e.target.value)}
-      />
-    </Wrapper>
-  );
+	return (
+		<Wrapper>
+			<Icon />
+			<Input
+				type="text"
+				value={value}
+				onChange={(e) => onChange(e.target.value)}
+			/>
+		</Wrapper>
+	);
 };
